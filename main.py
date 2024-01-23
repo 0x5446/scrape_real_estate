fa664b147b3a98f78707ca7585a4b90ae1e4e164
Frankee <spirittf@gmail.com>
2024-01-23 15:50:42 +0800

    update zf scrape logic

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from bs4 import BeautifulSoup
 import os, sys
 import csv
 import time
+import math
+import re
 
 
 
@@ -37,18 +39,45 @@ def configure_driver():
     return driver1, driver2
 
 
+# 配置WebDriver
+driver_zf, driver_esf = configure_driver()
 
 csv_zf = './output/zf.csv'
 url_zf_base = 'https://bj.zu.ke.com'
 
-def gen_url_zf_list(n: int):
-    return 'https://bj.zu.ke.com/zufang/' + ('pg' + str(n) if n > 1 else '') + 'rt200600000001#contentList'
+
+def get_all_zf_entrypoint():
+    base_url = 'https://bj.zu.ke.com/zufang/rt200600000001' 
+    driver_zf.get(base_url)
+    time.sleep(1)  # 等待页面加载
+    elements = driver_zf.find_elements(By.XPATH, "//li[@data-type='district']/a")
+
+    hrefs = []
+    links = []
+    # 遍历元素并获取href
+    for element in elements:
+        if element.text.strip() == '不限':
+            continue
+        href = element.get_attribute('href')
+        hrefs.append(href)
+
+    for href in hrefs:
+        driver_zf.get(href)
+        time.sleep(1)  # 等待页面加载
+        eles = driver_zf.find_elements(By.XPATH, "//li[@data-type='bizcircle']/a")
+        for e in eles:
+            if e.text.strip() == '不限':
+                continue
+            links.append(e.get_attribute('href'))
+    return links
 
 
 def parse_html(html):
     """解析HTML页面并提取所需数据"""
     soup = BeautifulSoup(html, 'html.parser')
     items = soup.find_all('div', class_='content__list--item')
+    if soup.find('div', class_='content__empty1'):
+        return []
     data = []
     for item in items:
         title_tag = item.find('p', class_='content__list--item--title')
@@ -106,32 +135,67 @@ def save_to_csv(data, file_path):
         for row in data:
             writer.writerow(row)
 
+def get_paged_url(url: str, n: int):
+    if n < 2:
+        return url
 
-# 配置WebDriver
-driver_zf, driver_esf = configure_driver()
+    parts = url.split('/')
+    # 确定要修改的部分的索引
+    index = -2 if parts[-1] == '' else -1
+
+    # 检查该部分是否已经以 'pg' 加数字开始
+    if re.match(r'^pg\d+', parts[index]):
+        # 仅替换 'pg' 后面的数字部分
+        parts[index] = re.sub(r'(?<=pg)\d+', str(n), parts[index])
+    else:
+        # 添加 'pg' 和数字
+        parts[index] = 'pg' + str(n) + parts[index]
+
+    # 重新组合 URL 部分
+    return '/'.join(parts)
 
 # 访问租房列表页面
 def scrape_zf():
-    n = 1
-    while True:
-        url_zf_list = gen_url_zf_list(n)
-        driver_zf.get(url_zf_list)
-        time.sleep(5)  # 等待页面加载
+    for url_zf_list in get_all_zf_entrypoint():
+        n = 1
 
+        driver_zf.get(url_zf_list)
+        time.sleep(1)
         # 获取页面源码
         html = driver_zf.page_source
 
         # 解析HTML并提取数据
         rental_data = parse_html(html)
 
-        if not rental_data:
-            print('Scrape ZF data completed!')
-            break;
-
         # 将数据保存到CSV文件
         save_to_csv(rental_data, csv_zf)
         
+
+        k = int(driver_zf.find_element(By.XPATH, '//span[@class="content__title--hl"]').text.strip())
+        m = math.ceil(k / 30)
+
+        print(f'Link: {url_zf_list}; {k} items, {m} pages to scrape')
         print(f'Scrape ZF data page:{n} sucessfully!')
         n = n + 1
+
+        while n <= m:
+            url_zf_list = get_paged_url(url_zf_list, n)
+            driver_zf.get(url_zf_list)
+            time.sleep(1)  # 等待页面加载
+
+            # 获取页面源码
+            html = driver_zf.page_source
+
+            # 解析HTML并提取数据
+            rental_data = parse_html(html)
+            if not rental_data:
+                print(f'Page {n}/{m} is empty, stop paging')
+                break 
+
+            # 将数据保存到CSV文件
+            save_to_csv(rental_data, csv_zf)
+            
+            print(f'Scrape ZF data page:{n} sucessfully!')
+            n = n + 1
 
 scrape_zf()
